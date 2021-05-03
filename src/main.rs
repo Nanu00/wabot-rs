7e3f193dc7f48db391138131dba16d7372b1b9a4
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2021-05-03 13:02:10 +0530

    Fixed framework related work

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
     framework::standard::{
-        CommandResult, macros::command,
-        StandardFramework
-    }.
+        CommandResult, macros::{command, group, help, check, hook},
+        StandardFramework, CommandGroup
+    },
 };
 use std::{
     env, process,
@@ -16,18 +16,26 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("Connected as {}", ready.user.name);
+    }
 }
+
+#[group]
+#[commands(ping)]
+struct General;
 
 #[tokio::main]
 async fn main() {
     
     let framework = StandardFramework::new()
-        .configure(|c| c.prefix("`"));
+        .configure(|c| c.prefix("`"))
+        .group(&GENERAL_GROUP);
 
     let mut bot = match Client::builder(&get_token())
         .event_handler(Handler)
-        .framework(framework).await
+        .framework(framework)
+        .await
         {
         Ok(c) => c,
         Err(e) => {
@@ -63,6 +71,7 @@ fn get_token() -> String {
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    println!("Ponging");
     msg.channel_id.say(&ctx.http, "Pong!").await?;
     Ok(())
 }
