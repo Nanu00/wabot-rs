70796a4850bfabb6fd9b75e7b6ee8c36eb19b8af
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2021-05-04 20:40:58 +0530

    Modified the AsciiMath functionality

--- src/botmods/math.rs
+++ src/botmods/math.rs
@@ -8,12 +8,13 @@
 // };
 
 use std::{
-    // process::Command,
+    process::Command,
     fs,
 };
 
 use tempfile::tempfile;
-use resvg;
+use usvg::SystemFontDB;
+use usvg;
 
 pub struct AsciiMath {
     raw: String,
@@ -21,13 +22,34 @@ pub struct AsciiMath {
 }
 
 impl AsciiMath {
-    async fn asmpng(asm: &str) -> Result<AsciiMath, Box<dyn std::error::Error>> {
-        let tmp = tempfile::tempfile()?;
-        let mut opt = resvg::usvg::Options::default();
+    pub fn asmpng(asm: &mut str) -> Result<AsciiMath, Box<dyn std::error::Error>> {
+        let mut asm = String::from(asm);
+        asm.insert(0, '"');
+        asm.push('"');
+
+        let tmp = tempfile()?;
+
+        let mut opt = usvg::Options::default();
+        opt.fontdb.load_system_fonts();
+        opt.fontdb.set_generic_families();
+
+        let mj_cli = Command::new("$(npm bin)/am2svg")
+            .arg(&asm)
+            .output()?;
+
+        let mj = String::from_utf8(mj_cli.stdout)?;
+
+        let svg_tree = usvg::Tree::from_str(&mj, &opt)?;
+
+        let pixmap_size = svg_tree.svg_node().size.to_screen_size();
+
+        let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
+
+        resvg::render(&svg_tree, usvg::FitTo::Original, pixmap.as_mut()).unwrap();
 
         Ok(
             AsciiMath {
-                raw: String::from(asm),
+                raw: asm,
                 file: tmp,
             }
         )
