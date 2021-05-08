0254f90ebedfa51b178a6f0397cd68e599e7c424
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2021-05-08 23:51:52 +0530

    Refractored code, made messages look better

--- src/botmods/math.rs
+++ src/botmods/math.rs
@@ -11,19 +11,86 @@ use std::{
     process::Command,
     borrow::Cow,
 };
-use tokio;
 #[allow(unused_imports)] use usvg::SystemFontDB;
 use usvg;
 use tiny_skia::Color;
-
-pub struct AsciiMath {
-    text: String,
-    byte_data: Vec<u8>
-}
 use tempfile;
 
 
+trait Math {
+    fn get_img_bytes(&self) -> &Vec<u8>;
+    fn get_plaintext(&self) -> &str;
+}
+
+
+fn svgpng(svg_data: &Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error + Sync + Send>> {
+        let mut opt = usvg::Options::default();
+        opt.fontdb.load_system_fonts();
+        opt.fontdb.set_generic_families();
+
+        let svg_tree = usvg::Tree::from_data(svg_data, &opt)?;
+        let pixmap_size = svg_tree.svg_node().size.to_screen_size();
+        let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width()*4, pixmap_size.height()*4).unwrap();
+        pixmap.fill(Color::BLACK);
+
+        // println!("Ready to render");
+
+        resvg::render(&svg_tree, usvg::FitTo::Zoom(4.0), pixmap.as_mut()).unwrap();
+
+        Ok(
+            pixmap.encode_png()?
+        )
+}
+
+
+async fn loading_msg(ctx: &Context, c_id: &serenity::model::id::ChannelId) -> Result<Message, SerenityError> {
+    c_id.send_message(&ctx.http, |m| {
+        m.content("Doing stuff");
+        m
+    }).await
+}
+
+
+async fn math_msg(ctx: &Context, c_id: &serenity::model::id::ChannelId, loading_msg: &Message, for_user: &serenity::model::user::User, math: impl Math) -> Result<Message, SerenityError> {
+    loading_msg.delete(&ctx.http).await?;
+
+    c_id.send_message(&ctx.http, |m|{
+        m.embed(|e| {
+            e.title("Math snippet");
+            e.description(format!("Input: {}", &math.get_plaintext()));
+            e.footer(|f| {
+                f.icon_url(for_user.avatar_url().unwrap());
+                f.text(format!("Requested by {}", for_user.name));
+                f
+            });
+            e
+        });
+        m.add_file(
+            http::AttachmentType::Bytes {
+                data: Cow::from(math.get_img_bytes()),
+                filename: String::from("testfile.png")
+            });
+        m
+    }).await
+}
+
+
+
 // AsciiMath
+
+pub struct AsciiMath {
+    text: String,
+    png_bytes: Vec<u8>
+}
+
+impl Math for AsciiMath {
+    fn get_img_bytes(&self) -> &Vec<u8> {
+        &self.png_bytes
+    }
+    fn get_plaintext(&self) -> &str {
+        &self.text
+    }
+}
 
 impl AsciiMath {
     pub fn asmpng(asm: &str) -> Result<AsciiMath, Box<dyn std::error::Error + Send + Sync>> {
@@ -31,36 +98,23 @@ impl AsciiMath {
         asm.insert(0, '\'');
         asm.push('\'');
 
-        println!("Made string: {}", asm);
+        // println!("Made string: {}", asm);
 
         let mj_cli = Command::new("sh")
             .arg("-c")
-            .arg(format!("~/node_modules/.bin/am2svg {}", &asm))
+            .arg(format!("~/node_modules/.bin/am2svg {} | sed -e 's/currentColor/white/g'", &asm))
             .output()?;
 
-        println!("Ran MathJax: {}", mj_cli.stdout.len());
+        // println!("Ran MathJax: {}", mj_cli.stdout.len());
 
-        let mut opt = usvg::Options::default();
-        opt.fontdb.load_system_fonts();
-        opt.fontdb.set_generic_families();
-
-        let svg_tree = usvg::Tree::from_data(&mj_cli.stdout, &opt)?;
-        let pixmap_size = svg_tree.svg_node().size.to_screen_size();
-        let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width()*4, pixmap_size.height()*4).unwrap();
-        pixmap.fill(Color::WHITE);
-
-        println!("Ready to render");
-
-        resvg::render(&svg_tree, usvg::FitTo::Zoom(4.0), pixmap.as_mut()).unwrap();
-
-        let png_raw = pixmap.encode_png()?;
+        let png_raw = svgpng(&mj_cli.stdout)?;
         
-        println!("Done");
+        // println!("Done");
 
         Ok(
             AsciiMath {
                 text: asm,
-                byte_data: png_raw,
+                png_bytes: png_raw,
             }
         )
     }
@@ -68,6 +122,9 @@ impl AsciiMath {
 
 #[command]
 pub async fn ascii(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+
+    let lm = loading_msg(ctx, &msg.channel_id).await?;
+
     let asm_raw = match args.remains() {
         Some(r) => Ok(r),
         None => Err("No arguments provided"),
@@ -75,15 +132,7 @@ pub async fn ascii(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
     let asm = AsciiMath::asmpng(asm_raw)?;
 
-    msg.channel_id.send_message(&ctx.http, |m| {
-        // m.content("Test");
-        m.add_file(
-            http::AttachmentType::Bytes {
-                data: Cow::from(asm.byte_data),
-                filename: String::from("testfile.png")
-            });
-        m
-    }).await?;
+    math_msg(ctx, &msg.channel_id, &lm, &msg.author, asm).await?;
 
     Ok(())
 }
@@ -93,61 +142,45 @@ pub async fn ascii(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 // Latex
 
 struct Latex {
-    latex: String,
-    png_data: Vec<u8>,
+    text: String,
+    png_bytes: Vec<u8>,
+}
+
+impl Math for Latex {
+    fn get_img_bytes(&self) -> &Vec<u8> {
+        &self.png_bytes
+    }
+    fn get_plaintext(&self) -> &str {
+        &self.text
+    }
 }
 
 impl Latex {
     pub fn texpng(tex: &str) -> Result<Latex, Box<dyn std::error::Error + Send + Sync>> {
         let tex_dir = tempfile::TempDir::new()?;
 
-        let pre = "\\documentclass{standalone}
-    \\usepackage{amsmath}
-    \\begin{document}
-    \\[
-        ";
-
-        let post = "
-            \\]
-            \\end{document}";
-
-        println!("tex string {}", tex);
+        // println!("tex string {}", tex);
 
         let _dvitex_cli = Command::new("sh")
             .arg("-c")
             .arg(format!("latex -jobname=texput -output-directory={} '\\documentclass[preview,margin=1pt]{{standalone}} \\usepackage[utf8]{{inputenc}} \\usepackage{{mathtools}} \\usepackage{{siunitx}} \\usepackage[version=4]{{mhchem}} \\usepackage{{amsmath}} \\usepackage{{xcolor}} \\begin{{document}} \\color{{white}} \\begin{{equation*}} {} \\end{{equation*}} \\end{{document}}'", &tex_dir.path().to_str().unwrap(), &tex))
             .output()?;
 
-        println!("dvi made {}\n{}", &tex_dir.path().join("texput.dvi").to_str().unwrap(), String::from_utf8(_dvitex_cli.stdout.clone()).unwrap());
+        // println!("dvi made {}\n{}", &tex_dir.path().join("texput.dvi").to_str().unwrap(), String::from_utf8(_dvitex_cli.stdout.clone()).unwrap());
 
         let dvisvg_cli = Command::new("sh")
             .arg("-c")
             .arg(format!("dvisvgm --page=1- -n --bbox=\"5pt\" -s {}", &tex_dir.path().join("texput.dvi").to_str().unwrap()))
             .output()?;
 
-        println!("svg made");
+        // println!("svg made");
 
-        let mut opt = usvg::Options::default();
-        opt.fontdb.load_system_fonts();
-        opt.fontdb.set_generic_families();
-
-        let svg_tree = usvg::Tree::from_data(&dvisvg_cli.stdout, &opt)?;
-        let pixmap_size = svg_tree.svg_node().size.to_screen_size();
-        let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width()*4, pixmap_size.height()*4).unwrap();
-        pixmap.fill(Color::BLACK);
-
-        println!("ready to render");
-
-        resvg::render(&svg_tree, usvg::FitTo::Zoom(4.0), pixmap.as_mut()).unwrap();
-
-        println!("rendered");
-
-        let png_raw = pixmap.encode_png()?;
+        let png_raw = svgpng(&dvisvg_cli.stdout)?;
 
         Ok(
             Latex {
-                latex: String::from(tex),
-                png_data: png_raw,
+                text: String::from(tex),
+                png_bytes: png_raw,
             }
             )
     }
@@ -155,6 +188,9 @@ impl Latex {
 
 #[command]
 pub async fn latex(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+
+    let lm = loading_msg(ctx, &msg.channel_id).await?;
+
     let latex_raw = match args.remains() {
         Some(r) => Ok(r),
         None => Err("No arguments provided"),
@@ -162,17 +198,19 @@ pub async fn latex(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
     let latex = Latex::texpng(latex_raw)?;
 
-    msg.channel_id.send_message(&ctx.http, |m| {
-        // m.content("Test");
-        m.add_file(
-            http::AttachmentType::Bytes {
-                data: Cow::from(latex.png_data),
-                filename: String::from("testfile.png")
-            }
-            // http::AttachmentType::File {file: &latex.png_file, filename: String::from("test.png")}
-            );
-        m
-    }).await?;
+    math_msg(ctx, &msg.channel_id, &lm, &msg.author, latex).await?;
+
+    // msg.channel_id.send_message(&ctx.http, |m| {
+    //     // m.content("Test");
+    //     m.add_file(
+    //         http::AttachmentType::Bytes {
+    //             data: Cow::from(latex.png_bytes),
+    //             filename: String::from("testfile.png")
+    //         }
+    //         // http::AttachmentType::File {file: &latex.png_file, filename: String::from("test.png")}
+    //         );
+    //     m
+    // }).await?;
 
     Ok(())
 }
