c3256e51ea23efbbbaa3a0bc27411a7d9a1b7eea
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2021-05-09 09:50:36 +0530

    Small fix

--- src/botmods/math.rs
+++ src/botmods/math.rs
@@ -58,6 +58,7 @@ async fn math_msg(ctx: &Context, c_id: &serenity::model::id::ChannelId, loading_
         m.embed(|e| {
             e.title("Math snippet");
             e.description(format!("Input: {}", &math.get_plaintext()));
+            e.image("attachment://testfile.png");
             e.footer(|f| {
                 f.icon_url(for_user.avatar_url().unwrap());
                 f.text(format!("Requested by {}#{}", for_user.name, for_user.discriminator));
