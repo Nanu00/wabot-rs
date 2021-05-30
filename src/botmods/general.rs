a9bdf46585c018bc5e7f3fbb564ad6033929f27e
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2021-05-30 23:37:51 +0530

    Updated about command

--- src/botmods/general.rs
+++ src/botmods/general.rs
@@ -18,9 +18,10 @@ pub async fn about(ctx: &Context, msg: &Message) -> CommandResult {
     msg.channel_id.send_message(&ctx.http, |m| {
                 m.embed(|e| {
                     e.title("About wabot");
-                    e.description("A simple discord bot to fetch steps from wolfram|alpha");
+                    e.description("A Discord bot to compile Latex/AsciiMath snippets~~ and fetch steps from Wolfram|Alpha~~, written in Rust");
                     e.image("https://cdn.discordapp.com/avatars/787599246136311848/a8d00cc4e9d36e2babaef362172f7085.png?size=128");
                     e.field("Made by", "Nanu#3294", false);
+                    e.field("Check out the github repo!", "[Nanu00/wabot-rs](https://github.com/Nanu00/wabot-rs)", false);
                     e
                 });
                 m
