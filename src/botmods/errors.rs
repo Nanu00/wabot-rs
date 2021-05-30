f08961d24dc99ee78f78005ec94ce14af412382f
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2021-05-30 21:43:40 +0530

    Error handling overhaul

---
+++ src/botmods/errors.rs
@@ -0,0 +1,55 @@
+use std::{
+    error::Error as StdErr,
+    fmt::Display,
+    fmt::Debug,
+    fmt,
+    io,
+};
+use usvg;
+use png;
+
+#[derive(Debug)]
+pub enum Error {
+    SVGError(usvg::Error),
+    PNGError(png::EncodingError),
+    IOError(io::Error),
+}
+
+impl From<usvg::Error> for Error {
+    fn from(e: usvg::Error) -> Error {
+        Error::SVGError(e)
+    }
+}
+
+impl From<png::EncodingError> for Error {
+    fn from(e: png::EncodingError) -> Error {
+        Error::PNGError(e)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Error {
+        Error::IOError(e)
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::SVGError(e) => f.write_str(&format!("Error making the SVG: {}", e)),
+            Error::PNGError(e) => f.write_str(&format!("Error making the PNG: {}", e)),
+            Error::IOError(e) => f.write_str(&format!("I/O error: {}", e)),
+        }
+    }
+}
+
+impl StdErr for Error {
+    fn source(&self) -> Option<&(dyn StdErr + 'static)> {
+        match self {
+            Error::SVGError(inner) => Some(inner),
+            Error::PNGError(inner) => Some(inner),
+            Error::IOError(inner) => Some(inner),
+            _ => None,
+        }
+    }
+}
